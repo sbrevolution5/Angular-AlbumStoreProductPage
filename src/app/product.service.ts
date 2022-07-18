ed534298b058ce7d5c36d2a62a3811b098633f31
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-18 12:09:35 -0400

    map response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operator/map';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
@@ -9,6 +9,6 @@ export class ProductService {
     
   }
   getAlbum(id:number){
-    return this._http.get(this._albumUrl).pipe()
+    return this._http.get(this._albumUrl).map((response:Response) => response.json());
   }
 }
