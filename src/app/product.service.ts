d968e548e6d77633a798f9e7af6742af392cbe0a
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-18 15:17:31 -0400

    return type

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,6 +11,6 @@ export class ProductService {
     
   }
   getAlbum(id:number):Observable<Album> {
-    return this._http.get(this._albumUrl).map((response:Response) => <Album>response.json());
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
