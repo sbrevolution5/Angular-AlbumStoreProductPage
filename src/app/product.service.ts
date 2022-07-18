58ff20852a6ea47b26f64872b27c1909c4a87e51
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-18 11:48:15 -0400

    implement getalbum

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operator/map';
 @Injectable()
 export class ProductService {
 
-  constructor() { }
-
+  private _albumUrl :string = "../assets/album.json"; 
+  constructor(private _http:Http) {
+    
+  }
+  getAlbum(id:number){
+    return this._http.get(this._albumUrl).pipe()
+  }
 }
