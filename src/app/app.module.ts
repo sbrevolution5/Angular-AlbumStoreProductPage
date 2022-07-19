89d7dc9b1860e563a93b1f16bc5680e1f7698c60
sbrevolution5 <Sbrevolution5@aim.com>
2022-07-19 10:28:30 -0400

    add clickable links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 const appRoutes:Routes=[
   {path:"products",component:ProductListComponent },
-  {path:"product/:id",component:ProductPageComponent}];
+  {path:"product/:id",component:ProductPageComponent},
+{path:'',redirectTo:"products",pathMatch:"full"}];
 @NgModule({
   declarations: [
     AppComponent,
